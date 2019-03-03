309dedfb7b93023677cf2f09af469068041f6e87
shark27 <44381346+jslzr27@users.noreply.github.com>
2019-03-03 14:32:54 -0500

    added functionality

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE `burgers_db`;
+
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Triple Cheese', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Chuleton', false);
